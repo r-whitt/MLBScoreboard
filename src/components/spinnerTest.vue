328d3f611957933dcce397c2e4ca54667ef2d947
r-whitt <rwhitt85@gmail.com>
2018-06-11 08:16:53 -0700

    using Spinner Example Page, adding working spinners

--- src/components/spinnerTest.vue
+++ src/components/spinnerTest.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="jumbotron" id="imgWrapper">
       <p>Testing</p>
+      <!-- to make wheel appear/disappear change the id -->
+      <div id="loader" class="img-responsive center-block"></div>
+      <div id="loadText">
+        <p>Loading....</p>
+      </div>
+      <br>
+      <br>
+      <br>
+      <br> 
+      <img src="../assets/wrench2.jpg" id="loader2" class="img-responsive center-block"/>
   </div>
 </template>
 
@@ -13,12 +23,34 @@ export default {
 </script>
 
 <style>
-#pic {
+#imgWrapper {
+    background-color:white;
+}
+#loader {
+    border: 16px solid #f3f3f3; /* Light grey */
+    border-top: 16px solid #221faf; /*Blue*/
+    border-radius: 50%; /*makes the image a wheel*/
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite; /*can name 1st pos. ('spin') differently, just ref. @keyframe. This determines speed, duration, action, etc*/
     display: block;
     margin-left: auto;
     margin-right: auto;
 }
-#imgWrapper {
-    background-color:white;
+#loader2 {
+    border-radius: 20%;
+    animation: spin 2s linear infinite;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+
+}
+#loadText {
+    margin: auto;
+    display: inline-block;
+}
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
 }
 </style>
